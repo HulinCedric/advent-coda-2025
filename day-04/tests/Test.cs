0c64c2abdf663034926b003f3e20129416b6bf84
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-05 09:26:45 +0100

    test: enhance todo test to validate file content and line count

--- day-04/tests/Test.cs
+++ day-04/tests/Test.cs
@@ -6,7 +6,16 @@ namespace Day04.Tests;
 public class Test
 {
     [Fact]
-    public void todo() => FileContent("sample").Should().Contain("Pepin");
+    public void todo()
+    {
+        var fileContent = FileContent("sample");
+
+        fileContent.Should().Contain("Pepin");
+        
+        fileContent.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries)
+            .Length.Should()
+            .Be(5);
+    }
 
     private static string FileContent(string fileName) => File.ReadAllText(fileName);
 }
