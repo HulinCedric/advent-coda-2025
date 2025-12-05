73885229f4e211cb7ced448bfd6084dd046eef64
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-05 10:38:07 +0100

    test: update Get_top3 test to specify data source and add new test for top 3 elves from data

--- day-04/tests/Test.cs
+++ day-04/tests/Test.cs
@@ -7,7 +7,7 @@ namespace Day04.Tests;
 public class Test
 {
     [Fact]
-    public void Get_top3()
+    public void Get_top3_from_sample()
         => ElvesFrom(fileName: "sample")
             .ComputeReport()
             .Should()
@@ -16,5 +16,17 @@ public class Test
                 🍪 Elf of the Day: Nora with 24000 calories!
                 🥈 Then comes Marius (11000) and Tika (10000)
                 🎁 Combined snack power of Top 3: 45000 calories!
+                """);
+    
+    [Fact]
+    public void Get_top3()
+        => ElvesFrom(fileName: "data")
+            .ComputeReport()
+            .Should()
+            .Be(
+                """
+                🍪 Elf of the Day: Susanoo with 57177 calories!
+                🥈 Then comes Maeve (52791) and Set (52573)
+                🎁 Combined snack power of Top 3: 162541 calories!
                 """);
 }
