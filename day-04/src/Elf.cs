b0f7636bce1c6632be49769e3bc8ffcbbe8ba6b9
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-05 18:50:26 +0100

    refactor: change Calories property to IReadOnlyList for better encapsulation

--- day-04/src/Elf.cs
+++ day-04/src/Elf.cs
@@ -1,6 +1,6 @@
 ﻿namespace Day04;
 
-public record Elf(string Name, List<int> Calories)
+public record Elf(string Name, IReadOnlyList<int> Calories)
 {
     public int GetTotalCalories() => Calories.Sum();
 }
