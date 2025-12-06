333c07046c3220fc489ff6b98328b8c1b0751fa9
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-06 09:16:45 +0100

    refactor: Simplify design to avoid to be error prone

--- day-06/tests/AverageWeightCalculatorShould.cs
+++ day-06/tests/AverageWeightCalculatorShould.cs
@@ -9,31 +9,32 @@ public class AverageWeightCalculatorShould(ITestOutputHelper testOutputHelper)
 {
     [Theory]
     [ClassData(typeof(AverageWeightScenarios))]
-    public void Compute_average_weight(decimal[] weights, int length, decimal expected)
+    public void Compute_average_weight(decimal[] weights,  decimal expected)
     {
-        var averageWeight = AverageWeight(weights, length);
+        var averageWeight = AverageWeight(weights);
 
-        testOutputHelper.WriteLine($"Average weight for {length} gifts: {averageWeight:F2}");
+        testOutputHelper.WriteLine($"Average weight for {weights.Length} gifts: {averageWeight:F2}");
 
         averageWeight.Should().BeApproximately(expected, 0.01m);
     }
 
-    private class AverageWeightScenarios : TheoryData<decimal[], int, double>
+    private class AverageWeightScenarios : TheoryData<decimal[], decimal>
     {
         public AverageWeightScenarios()
         {
-            Add([2m, 5m, 7m, 10m], 4, 6.00);
-            Add([2m], 1, 2.00);
-            Add([], 0, 0.00);
-            Add([1m, 2m], 2, 1.50);
+            Add([2m, 5m, 7m, 10m], 6.00m);
+            Add([2m], 2.00m);
+            Add([], 0.00m);
+            Add([1m, 2m], 1.50m);
         }
     }
 }
 
 public static class WeightCalculator
 {
-    public static decimal AverageWeight(decimal[] weights, int numberOfWeights)
+    public static decimal AverageWeight(decimal[] weights)
     {
+        var numberOfWeights = weights.Length;
         if (numberOfWeights == 0) return 0.0m;
 
         return weights.Sum() / numberOfWeights;
