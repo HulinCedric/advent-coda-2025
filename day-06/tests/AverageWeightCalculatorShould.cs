d1277da7d3f2e0f2430492718dd4dd0295e70d88
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-06 09:24:58 +0100

    refactor AverageWeightCalculatorShould: remove unused AverageWeightScenarios class

--- day-06/tests/AverageWeightCalculatorShould.cs
+++ day-06/tests/AverageWeightCalculatorShould.cs
@@ -17,15 +17,4 @@ public class AverageWeightCalculatorShould(ITestOutputHelper testOutputHelper)
 
         averageWeight.Should().BeApproximately(expected, 0.01);
     }
-
-    private class AverageWeightScenarios : TheoryData<double[], double>
-    {
-        public AverageWeightScenarios()
-        {
-            Add([2, 5, 7, 10], 6.00);
-            Add([2], 2.00);
-            Add([], 0.00);
-            Add([1, 2], 1.50);
-        }
-    }
 }
