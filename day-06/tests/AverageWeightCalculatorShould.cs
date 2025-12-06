429792f3ce04947f8ff8369efb841a92c6bed403
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-06 09:27:10 +0100

    refactor AverageWeightCalculatorShould: simplify Compute_average_weight method

--- day-06/tests/AverageWeightCalculatorShould.cs
+++ day-06/tests/AverageWeightCalculatorShould.cs
@@ -1,20 +1,15 @@
 using FluentAssertions;
 using Xunit;
-using Xunit.Abstractions;
 using static Day06.WeightCalculator;
 
 namespace Day06.Tests;
 
-public class AverageWeightCalculatorShould(ITestOutputHelper testOutputHelper)
+public class AverageWeightCalculatorShould
 {
     [Theory]
     [ClassData(typeof(AverageWeightScenarios))]
-    public void Compute_average_weight(double[] weights, double expectedAverageWeight)
-    {
-        var averageWeight = AverageWeight(weights);
-
-        testOutputHelper.WriteLine($"Average weight for {weights.Length} gifts: {averageWeight:F2}");
-
-        averageWeight.Should().BeApproximately(expectedAverageWeight, 0.01);
-    }
+    public void Compute_average_weight(double[] weights, double averageWeight)
+        => AverageWeight(weights)
+            .Should()
+            .BeApproximately(averageWeight, 0.01);
 }
