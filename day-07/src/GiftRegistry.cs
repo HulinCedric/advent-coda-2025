2d4e824544d862a41019a871c8ffa8daba894e6f
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-07 18:19:06 +0100

    SonarLint: Types should be defined in named namespaces

--- day-07/src/GiftRegistry.cs
+++ day-07/src/GiftRegistry.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;using System;
-using System.Collections.Generic;using System;
-using System.Collections.Generic;using System;
-using System.Collections.Generic;using System;
-using System.Collections.Generic;
+namespace Registry;
 
 public class GiftRegistry
 {
