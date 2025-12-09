3efcba38ce70ddcb7e432874d3f15af6e40ae985
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 09:25:52 +0100

    test: update TODO test to validate Elvishcoordinates loading from trace file

--- day-09/tests/SantaJourneyCalculatorShould.cs
+++ day-09/tests/SantaJourneyCalculatorShould.cs
@@ -9,22 +9,29 @@ public class SantaJourneyCalculatorShould
     [Fact]
     public void TODO()
     {
-        var lines = File.ReadAllLines("trace");
+        var elvishcoordinates = LoardElvishcoordinates("trace");
+        elvishcoordinates
+            .First()
+            .Should()
+            .BeEquivalentTo(new Elvishcoordinates(146, 7714456.11274088m, 5639226.707649254m));
+    }
+
+    private static IEnumerable<Elvishcoordinates> LoardElvishcoordinates(string trace)
+    {
+        var lines = File.ReadAllLines(trace);
         lines.Length.Should().Be(500);
 
         lines.First().Should().BeEquivalentTo("146,7714456.11274088,5639226.707649254");
 
-        lines.Select(line =>
-            {
-                var part = line.Split(',');
-                return new Elvishcoordinates(
-                    int.Parse(part[0], CultureInfo.InvariantCulture),
-                    decimal.Parse(part[1], CultureInfo.InvariantCulture),
-                    decimal.Parse(part[2], CultureInfo.InvariantCulture));
-            })
-            .First()
-            .Should()
-            .BeEquivalentTo(new Elvishcoordinates(146, 7714456.11274088m, 5639226.707649254m));
+        var elvishcoordinates = lines.Select(line =>
+        {
+            var part = line.Split(',');
+            return new Elvishcoordinates(
+                int.Parse(part[0], CultureInfo.InvariantCulture),
+                decimal.Parse(part[1], CultureInfo.InvariantCulture),
+                decimal.Parse(part[2], CultureInfo.InvariantCulture));
+        });
+        return elvishcoordinates;
     }
 }
 
