6eaf85863bac4ebb0645a90432d3b423f64f9cb5
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 09:18:28 +0100

    test: enhance TODO test to validate trace file content and structure

--- day-09/tests/SantaJourneyCalculatorShould.cs
+++ day-09/tests/SantaJourneyCalculatorShould.cs
@@ -8,6 +8,11 @@ public class SantaJourneyCalculatorShould
     [Fact]
     public void TODO()
     {
-        File.ReadAllLines("trace").Length.Should().Be(500);
+        var lines = File.ReadAllLines("trace");
+        lines.Length.Should().Be(500);
+        
+        lines.First().Should().BeEquivalentTo("146,7714456.11274088,5639226.707649254");
+        
+        lines.Select(line => line.Split(',')).First().Should().BeEquivalentTo("146", "7714456.11274088", "5639226.707649254");
     }
 }
