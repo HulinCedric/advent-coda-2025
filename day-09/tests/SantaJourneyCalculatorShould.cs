7b3833c659cb95b5716b3a4df3af055bc0f8372a
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 09:50:31 +0100

    feat: add conversion from Elvish coordinates to WGS84 and update coordinate types to double

--- day-09/tests/SantaJourneyCalculatorShould.cs
+++ day-09/tests/SantaJourneyCalculatorShould.cs
@@ -13,7 +13,24 @@ public class SantaJourneyCalculatorShould
         elvishcoordinates.Count().Should().Be(500);
         elvishcoordinates.First()
             .Should()
-            .BeEquivalentTo(new Elvishcoordinates(146, 7714456.11274088m, 5639226.707649254m));
+            .BeEquivalentTo(new Elvishcoordinates(146, 7714456.11274088d, 5639226.707649254d));
+    }
+
+    // EPSG:3857 => WGS84  WGS84 (longitude/latitude en degrés)
+    // R = 6378137 (m).
+    // lon_deg = (x_m / R) * 180/π
+    // lat_deg = (2 * atan(exp(y_m / R)) - π/2) * 180/π
+    [Fact]
+    public void Convert_elvish_coordinate_to_WGS84()
+    {
+        var elvishCoordinate = new Elvishcoordinates(146, 7714456.11274088d, 5639226.707649254d);
+
+        const double R = 6378137d;
+        var lonDeg = (elvishCoordinate.XInMeter / R) * 180d / Math.PI;
+        var latDeg = (2d * Math.Atan(Math.Exp(elvishCoordinate.YInMeter / R)) - Math.PI / 2d) * 180d / Math.PI;
+
+        lonDeg.Should().BeApproximately(69.30013834744402d, 0.0000001d);
+        latDeg.Should().BeApproximately(45.11235404506074d, 0.0000001d);
     }
 
     private static IEnumerable<Elvishcoordinates> LoadElvishCoordinates(string fileName)
@@ -28,9 +45,12 @@ public class SantaJourneyCalculatorShould
         var part = line.Split(',');
         return new Elvishcoordinates(
             int.Parse(part[0], CultureInfo.InvariantCulture),
-            decimal.Parse(part[1], CultureInfo.InvariantCulture),
-            decimal.Parse(part[2], CultureInfo.InvariantCulture));
+            double.Parse(part[1], CultureInfo.InvariantCulture),
+            double.Parse(part[2], CultureInfo.InvariantCulture));
     }
 }
 
-public sealed record Elvishcoordinates(int Order, decimal XInMeter, decimal YInMeter);
+/// <summary>
+///     Coordinates in metres (Elfic map – Web Mercator / EPSG:3857)
+/// </summary>
+public sealed record Elvishcoordinates(int Order, double XInMeter, double YInMeter);
