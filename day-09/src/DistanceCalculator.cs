6ded967bfb0ac0e40451faef509d60ba10ca9df3
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 18:37:47 +0100

    refactor: enhance distance calculation by extracting Haversine formula into a separate method

--- day-09/src/DistanceCalculator.cs
+++ day-09/src/DistanceCalculator.cs
@@ -9,19 +9,29 @@ public static class DistanceCalculator
     /// <seealso href="https://en.wikipedia.org/wiki/Haversine_formula" />
     public static double DistanceInKm(Wgs84Coordinate coord1, Wgs84Coordinate coord2)
     {
+        // Convert degrees to radians
         var lat1 = ToRadius(coord1.LatitudeInDegrees);
         var lon1 = ToRadius(coord1.LongitudeInDegrees);
         var lat2 = ToRadius(coord2.LatitudeInDegrees);
         var lon2 = ToRadius(coord2.LongitudeInDegrees);
+
+        // Distance calculation
         var dlat = lat2 - lat1;
         var dlon = lon2 - lon1;
+
+        // Haversine formula
+        return HaversineFormula(dlat, lat1, lat2, dlon);
+    }
+
+    private static double HaversineFormula(double dlat, double lat1, double lat2, double dlon)
+    {
         var a = Math.Pow(Math.Sin(dlat / 2d), 2) +
                 Math.Cos(lat1) *
                 Math.Cos(lat2) *
                 Math.Pow(Math.Sin(dlon / 2d), 2);
         var c = 2d * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1d - a));
-        var distanceInKm = EarthMeanRadiusInKilometer * c;
-        return distanceInKm;
+
+        return EarthMeanRadiusInKilometer * c;
     }
 
     private static double ToRadius(double input) => input * Math.PI / 180d;
