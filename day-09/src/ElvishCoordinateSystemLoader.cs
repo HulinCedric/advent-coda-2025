02c4ee04c0d804a9ddb52c5180ca06207f832db2
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 18:42:51 +0100

    refactor: simplify Elvish coordinate loading by streamlining file reading and mapping logic

--- day-09/src/ElvishCoordinateSystemLoader.cs
+++ day-09/src/ElvishCoordinateSystemLoader.cs
@@ -5,19 +5,16 @@ namespace Day09;
 public static class ElvishCoordinateSystemLoader
 {
     public static IEnumerable<ElvishCoordinate> LoadElvishCoordinates(string fileName)
-    {
-        var lines = File.ReadAllLines(fileName);
+        => File.ReadAllLines(fileName)
+            .Select(ExtractPart)
+            .Select(ElvishCoordinate);
 
-        return lines.Select(MapElvishCoordinate);
-    }
+    private static string[] ExtractPart(string line) => line.Split(',');
 
-    private static ElvishCoordinate MapElvishCoordinate(string line)
-    {
-        var part = line.Split(',');
-        return new ElvishCoordinate(
+    private static ElvishCoordinate ElvishCoordinate(string[] part)
+        => new(
             int.Parse(part[0], CultureInfo.InvariantCulture),
             new Epsg3857Coordinate(
                 double.Parse(part[1], CultureInfo.InvariantCulture),
                 double.Parse(part[2], CultureInfo.InvariantCulture)));
-    }
 }
