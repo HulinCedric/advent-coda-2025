f9479b7411d6a6da316fb8d7b22118464dd92694
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 09:24:55 +0100

    feat: enhance trace file test to validate Elvishcoordinates parsing

--- day-09/tests/SantaJourneyCalculatorShould.cs
+++ day-09/tests/SantaJourneyCalculatorShould.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using FluentAssertions;
 using Xunit;
 
@@ -10,9 +11,21 @@ public class SantaJourneyCalculatorShould
     {
         var lines = File.ReadAllLines("trace");
         lines.Length.Should().Be(500);
-        
+
         lines.First().Should().BeEquivalentTo("146,7714456.11274088,5639226.707649254");
-        
-        lines.Select(line => line.Split(',')).First().Should().BeEquivalentTo("146", "7714456.11274088", "5639226.707649254");
+
+        lines.Select(line =>
+            {
+                var part = line.Split(',');
+                return new Elvishcoordinates(
+                    int.Parse(part[0], CultureInfo.InvariantCulture),
+                    decimal.Parse(part[1], CultureInfo.InvariantCulture),
+                    decimal.Parse(part[2], CultureInfo.InvariantCulture));
+            })
+            .First()
+            .Should()
+            .BeEquivalentTo(new Elvishcoordinates(146, 7714456.11274088m, 5639226.707649254m));
     }
 }
+
+public sealed record Elvishcoordinates(int Order, decimal XInMeter, decimal YInMeter);
