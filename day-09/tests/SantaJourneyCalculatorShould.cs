fc3458ac9e57ef426304cbf118450faed5023ec3
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 10:13:21 +0100

    refactor: update Load_elvish_coordinates_from_file to assert count and use MinBy for order

--- day-09/tests/SantaJourneyCalculatorShould.cs
+++ day-09/tests/SantaJourneyCalculatorShould.cs
@@ -10,19 +10,20 @@ public class SantaJourneyCalculatorShould
     public void Load_elvish_coordinates_from_file()
     {
         var elvishcoordinates = LoadElvishCoordinates("trace");
+
         elvishcoordinates.Count().Should().Be(500);
+
         elvishcoordinates
-            .MinBy(c=>c.Order)
+            .MinBy(c => c.Order)
             .Should()
             .BeEquivalentTo(
                 new ElvishCoordinate(1, new WebMercatorCoordinate(253716.21038051567d, 6234041.976798748d)));
-        
+
         elvishcoordinates
-            .MaxBy(c=>c.Order)
+            .MaxBy(c => c.Order)
             .Should()
             .BeEquivalentTo(
                 new ElvishCoordinate(500, new WebMercatorCoordinate(16826950.84323861d, -4016067.571985976d)));
-
     }
 
     [Fact]
