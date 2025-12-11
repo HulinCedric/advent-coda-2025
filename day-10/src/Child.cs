6edbbdae584e5585dafc4e9184eb91904599594a
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-11 13:44:45 +0100

    refactor(child): introduce Behavior enum and separate GiftRequests into its own file

--- day-10/src/Child.cs
+++ day-10/src/Child.cs
@@ -1,27 +1,5 @@
 namespace GiftSelection;
 
-public enum Behavior
-{
-    Naughty,
-    Normal,
-    Nice
-}
-
-public sealed record GiftRequests(List<GiftRequest> Requests)
-{
-    internal string? LastFeasibleGift() => Requests
-        .Where(gift => gift.IsFeasible)
-        .Reverse()
-        .Select(gift => gift.GiftName)
-        .FirstOrDefault();
-
-    internal string? FirstFeasibleGift() => Requests
-        .Where(gift => gift.IsFeasible)
-        .Select(gift => gift.GiftName)
-        .FirstOrDefault();
-
-}
-
 public sealed record Child(
     string FirstName,
     string LastName,
---
+++ day-10/src/GiftRequests.cs
@@ -0,0 +1,16 @@
+﻿namespace GiftSelection;
+
+public sealed record GiftRequests(List<GiftRequest> Requests)
+{
+    internal string? LastFeasibleGift() => Requests
+        .Where(gift => gift.IsFeasible)
+        .Reverse()
+        .Select(gift => gift.GiftName)
+        .FirstOrDefault();
+
+    internal string? FirstFeasibleGift() => Requests
+        .Where(gift => gift.IsFeasible)
+        .Select(gift => gift.GiftName)
+        .FirstOrDefault();
+
+}
