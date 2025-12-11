dc51ffd5c2598e6061fd5bb7fec057b93a8c306c
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-11 20:12:53 +0100

    refactor: unify calculation strategy types by using a function delegate alias

--- day-11/src/Building.cs
+++ day-11/src/Building.cs
@@ -1,15 +1,17 @@
 ﻿namespace Navigation;
 
+using CalculationStrategy = Func<char, int>;
+
 public static class Building
 {
     public static int WhichFloor(string signalStream) => signalStream.Sum(Calculate(signalStream));
 
-    private static Func<char, int> Calculate(string signalStream)
+    private static CalculationStrategy Calculate(string signalStream)
         => signalStream.Contains("🧝")
             ? EflCalculationStrategy()
             : StandardCalculationStrategy();
 
-    private static Func<char, int> StandardCalculationStrategy()
+    private static CalculationStrategy StandardCalculationStrategy()
         => signal => signal switch
         {
             '(' => 1,
@@ -17,7 +19,7 @@ public static class Building
             _ => 0
         };
 
-    private static Func<char, int> EflCalculationStrategy()
+    private static CalculationStrategy EflCalculationStrategy()
         => signal => signal switch
         {
             ')' => 3,
