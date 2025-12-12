35b81dac5d5282c785f15f249aa2ea951170cc26
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-12 08:16:10 +0100

    refactor: improve calculation strategies by using constants for signal characters

--- day-11/src/Building.cs
+++ day-11/src/Building.cs
@@ -4,26 +4,29 @@ using CalculationStrategy = Func<char, int>;
 
 public static class Building
 {
+    private const char Up = '(';
+    private const char Down = ')';
+    private const string Elf = "🧝";
     public static int WhichFloor(string signalStream) => signalStream.Sum(Calculate(signalStream));
 
     private static CalculationStrategy Calculate(string signalStream)
-        => signalStream.Contains("🧝")
+        => signalStream.Contains(Elf)
             ? EflCalculationStrategy()
             : StandardCalculationStrategy();
 
     private static CalculationStrategy StandardCalculationStrategy()
         => signal => signal switch
         {
-            '(' => 1,
-            ')' => -1,
+            Up => 1,
+            Down => -1,
             _ => 0
         };
 
     private static CalculationStrategy EflCalculationStrategy()
         => signal => signal switch
         {
-            ')' => 3,
-            '(' => -2,
+            Down => 3,
+            Up => -2,
             _ => 0
         };
 }
