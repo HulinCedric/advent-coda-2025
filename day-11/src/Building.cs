8e67d33afc26a362196c64db0fa7c35bf29fab32
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-12 09:40:14 +0100

    day11
    * setup
    
    * fix: update logic to handle parentheses correctly when "🧝" is present
    
    * refactor: remove dead code
    
    * refactor: Replace unused tuple by list
    
    * refactor: simplify sum calculation using LINQ
    
    * refactor: extract calculation logic into a separate method for clarity
    
    * refactor: streamline floor calculation by using LINQ for character processing
    
    * refactor: remove val accumulator but using Sum()
    
    * refactor: simplify WhichFloor method by removing unnecessary conditions
    
    * refactor: enhance calculation strategies by separating standard and Efl logic
    
    * refactor: simplify calculation strategies using switch expressions
    
    * refactor: optimize WhichFloor method by using function delegates for calculations
    
    * refactor: unify calculation strategy types by using a function delegate alias
    
    * refactor: improve calculation strategies by using constants for signal characters
    
    * refactor: correct signal handling in EflCalculationStrategy for upward movement
    
    * refactor: rename calculation strategy type for clarity and consistency
    
    * refactor: rename calculation strategy type for consistency and clarity
    
    * refactor: rename calculation method for clarity and consistency
    
    * refactor: simplify strategy definitions and improve readability

---
+++ day-11/src/Building.cs
@@ -0,0 +1,31 @@
+﻿namespace Navigation;
+
+using FloorStrategy = Func<char, int>;
+
+public static class Building
+{
+    private const char Up = '(';
+    private const char Down = ')';
+    private const string Elf = "🧝";
+
+    private static readonly FloorStrategy StandardStrategy = instruction => instruction switch
+    {
+        Up => 1,
+        Down => -1,
+        _ => 0
+    };
+
+    private static readonly FloorStrategy EflStrategy = instruction => instruction switch
+    {
+        Up => -2,
+        Down => 3,
+        _ => 0
+    };
+
+    public static int WhichFloor(string instructions) => instructions.Sum(FloorStrategy(instructions));
+
+    private static FloorStrategy FloorStrategy(string instructions)
+        => instructions.Contains(Elf)
+            ? EflStrategy
+            : StandardStrategy;
+}
