febfefc1dc68e5be55d64e50655a3c19dc91d669
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-12 08:22:44 +0100

    refactor: simplify strategy definitions and improve readability

--- day-11/src/Building.cs
+++ day-11/src/Building.cs
@@ -7,27 +7,25 @@ public static class Building
     private const char Up = '(';
     private const char Down = ')';
     private const string Elf = "🧝";
-    
+
+    private static readonly FloorStrategy StandardStrategy = instruction => instruction switch
+    {
+        Up => 1,
+        Down => -1,
+        _ => 0
+    };
+
+    private static readonly FloorStrategy EflStrategy = instruction => instruction switch
+    {
+        Up => -2,
+        Down => 3,
+        _ => 0
+    };
+
     public static int WhichFloor(string instructions) => instructions.Sum(FloorStrategy(instructions));
 
     private static FloorStrategy FloorStrategy(string instructions)
         => instructions.Contains(Elf)
-            ? EflStrategy()
-            : StandardStrategy();
-
-    private static FloorStrategy StandardStrategy()
-        => instruction => instruction switch
-        {
-            Up => 1,
-            Down => -1,
-            _ => 0
-        };
-
-    private static FloorStrategy EflStrategy()
-        => instruction => instruction switch
-        {
-            Up => -2,
-            Down => 3,
-            _ => 0
-        };
+            ? EflStrategy
+            : StandardStrategy;
 }
