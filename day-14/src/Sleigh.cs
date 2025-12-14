05973d0cfaabf65a5e12e0691beebd6b1906001d
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-14 19:05:32 +0100

    refactor: use immutable collection

--- day-14/src/Sleigh.cs
+++ day-14/src/Sleigh.cs
@@ -1,17 +1,19 @@
+using System.Collections.Immutable;
+
 namespace Day14;
 
 public record Sleigh
 {
     private readonly HouseLocation _houseLocation;
-    private readonly IReadOnlySet<HouseLocation> _visitedHouses;
+    private readonly ImmutableHashSet<HouseLocation> _visitedHouses;
 
-    private Sleigh(HouseLocation houseLocation, IReadOnlySet<HouseLocation> visitedHouses)
+    private Sleigh(HouseLocation houseLocation, ImmutableHashSet<HouseLocation> visitedHouses)
     {
         _houseLocation = houseLocation;
-        _visitedHouses = new HashSet<HouseLocation>(visitedHouses) { houseLocation };
+        _visitedHouses = visitedHouses.Add(houseLocation);
     }
 
-    public static Sleigh Start() => new(HouseLocation.StartingHouse(), new HashSet<HouseLocation>());
+    public static Sleigh Start() => new(HouseLocation.StartingHouse(), ImmutableHashSet.Create<HouseLocation>());
 
     public Sleigh FollowInstructions(string instructions)
         => instructions.Aggregate(this, (sleigh, instruction) => sleigh.MoveTo(instruction));
@@ -19,6 +21,6 @@ public record Sleigh
     public Sleigh MoveTo(char instruction) => new(_houseLocation.MoveTo(instruction), _visitedHouses);
 
     public HouseLocation CurrentHouse() => _houseLocation;
-    
+
     public int VisitedHousesNumber() => _visitedHouses.Count;
 }
