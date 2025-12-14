1ab3e0c4002f57ca8d9d3b95ae9b30b587064dc7
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-14 17:13:56 +0100

    test: first testing loop

--- day-14/tests/SantaShould.cs
+++ day-14/tests/SantaShould.cs
@@ -6,5 +6,24 @@ namespace Day14.Tests;
 public class SantaShould
 {
     [Fact]
-    public void Deliver_to_starting_house() => true.Should().BeFalse();
+    public void Deliver_to_starting_house()
+    {
+        // Arrange
+        var instructions = "NNESESW";
+
+        // Act
+        var uniqueHouses = DeliveryCounter.CountUniqueHouses(instructions);
+
+        // Assert
+        uniqueHouses.Should().Be(8);
+    }
 }
+
+public static class DeliveryCounter
+{
+    public static int CountUniqueHouses(string instructions)
+    {
+        return 1;
+    }
+}
+
