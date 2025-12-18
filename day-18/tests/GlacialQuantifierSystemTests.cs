7377df1b05398b479c214b38e39bfcb2af16acd8
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-18 21:27:54 +0100

    Add GQS operation calculation tests

--- day-18/tests/GlacialQuantifierSystemTests.cs
+++ day-18/tests/GlacialQuantifierSystemTests.cs
@@ -11,8 +11,22 @@ public class GlacialQuantifierSystemTests
     [InlineData('0', 0)]
     [InlineData('*', 1)]
     [InlineData('✦', 2)]
-    public void Translate_GQS_symbols(char symbol, int @decimal)
-        => Parse(symbol).Should().Be(@decimal);
+    public void Translate_GQS_symbols(char symbol, int @decimal) => Parse(symbol).Should().Be(@decimal);
+
+    [Theory]
+    [InlineData("✦0", 10)]
+    [InlineData("*", 1)]
+    [InlineData("❄", -1)]
+    [InlineData("☃", -2)]
+    [InlineData("✦**", 56)]
+    [InlineData("✦*0❄", 274)]
+    public void Calculate_GQS_operations(string operation, int @decimal) => Calculate(operation).Should().Be(@decimal);
+
+    private static int Calculate(string operation)
+        => operation
+            .Reverse()
+            .Select((symbol, index) => Parse(symbol) * (int)Math.Pow(5, index))
+            .Sum();
 
     private static int Parse(char symbol)
         => symbol switch
