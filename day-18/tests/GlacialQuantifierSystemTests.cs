ac52c3953da4166a4e4221e809396eeddcea3162
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-18 21:20:26 +0100

    Add tests for GQS symbol translation

--- day-18/tests/GlacialQuantifierSystemTests.cs
+++ day-18/tests/GlacialQuantifierSystemTests.cs
@@ -5,6 +5,22 @@ namespace GlacialQuantifierSystem.Tests;
 
 public class GlacialQuantifierSystemTests
 {
-    [Fact]
-    public void Todo() => false.Should().BeTrue();
+    [Theory]
+    [InlineData('☃', -2)]
+    [InlineData('❄', -1)]
+    [InlineData('0', 0)]
+    [InlineData('*', 1)]
+    [InlineData('✦', 2)]
+    public void Translate_GQS_symbols(char symbol, int @decimal)
+        => Parse(symbol).Should().Be(@decimal);
+
+    private static int Parse(char symbol)
+        => symbol switch
+        {
+            '☃' => -2,
+            '❄' => -1,
+            '0' => 0,
+            '*' => 1,
+            '✦' => 2
+        };
 }
