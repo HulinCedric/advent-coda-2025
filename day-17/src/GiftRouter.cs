af5e1d91f2e81f211467f56ff569332266205f36
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-18 09:39:32 +0100

    route table

--- day-17/src/GiftRouter.cs
+++ day-17/src/GiftRouter.cs
@@ -1,20 +1,26 @@
+using static Routing.GiftRoutes;
+
 namespace Routing;
+
+public static class GiftRoutes
+{
+    public const string Sled = "SLED";
+    public const string ReindeerExpress = "REINDEER-EXPRESS";
+    public const string WorkshopHold = "WORKSHOP-HOLD";
+    public const string Error = "ERROR";
+}
 
 public static class GiftRouter
 {
     public static string Route(Gift? gift)
         => gift switch
         {
-            null => "ERROR",
-            _ when gift.ZoneIsNotDefined() => "WORKSHOP-HOLD",
-            _ when gift.IsFragile() => RouteFragileGift(gift),
-            _ when gift.IsHeavy() => "SLED",
-            _ when gift.TargetExpressZone() => "REINDEER-EXPRESS",
-            _ => "SLED"
+            null => Error,
+            _ when gift.ZoneIsNotDefined() => WorkshopHold,
+            _ when gift.IsFragile() && gift.IsLight() => ReindeerExpress,
+            _ when gift.IsFragile() => Sled,
+            _ when gift.IsHeavy() => Sled,
+            _ when gift.TargetExpressZone() => ReindeerExpress,
+            _ => Sled
         };
-
-    private static string RouteFragileGift(Gift gift)
-        => gift.IsLight()
-            ? "REINDEER-EXPRESS"
-            : "SLED";
 }
