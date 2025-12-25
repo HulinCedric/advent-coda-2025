9206978379af6c222b85233a0db9b80dffb976e4
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-25 19:35:10 +0100

    refactor: simplify Country parsing logic using functional programming for improved readability

--- day-25/src/Country.cs
+++ day-25/src/Country.cs
@@ -1,14 +1,14 @@
 using LanguageExt;
+using static LanguageExt.Prelude;
 
 namespace GifFeedbackAudit;
 
 public record Country(string Value)
 {
     public static Option<Country> Parse(string input)
-    {
-        var trimmedInput = input?.Trim();
-        return string.IsNullOrWhiteSpace(trimmedInput) || !trimmedInput.All(char.IsLetter)
-            ? Option<Country>.None
-            : new Country(trimmedInput);
-    }
+        => Optional(input)
+            .Map(s => s.Trim())
+            .Filter(s => s.Length > 0)
+            .Filter(s => s.All(char.IsLetter))
+            .Map(s => new Country(s));
 }
