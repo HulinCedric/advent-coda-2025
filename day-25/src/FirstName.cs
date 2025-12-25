e15b4202643ae99cd24eea2f2a35db736dbdf7e5
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-25 19:36:31 +0100

    refactor: simplify FirstName parsing logic using functional programming for improved readability

--- day-25/src/FirstName.cs
+++ day-25/src/FirstName.cs
@@ -1,14 +1,16 @@
 using LanguageExt;
+using static LanguageExt.Prelude;
 
 namespace GifFeedbackAudit;
 
 public record FirstName(string Value)
 {
     public static Option<FirstName> Parse(string input)
-    {
-        var trimmedInput = input?.Trim();
-        return string.IsNullOrWhiteSpace(trimmedInput) || !trimmedInput.All(char.IsLetter)
-            ? Option<FirstName>.None
-            : new FirstName(trimmedInput);
-    }
+        => Optional(input)
+            .Map(s => s.Trim())
+            .Filter(s => s.Length > 0)
+            .Filter(ContainOnlyLetter)
+            .Map(s => new FirstName(s));
+
+    private static bool ContainOnlyLetter(string input) => input.All(char.IsLetter);
 }
