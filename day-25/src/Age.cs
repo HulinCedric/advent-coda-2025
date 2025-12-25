ec85617a6813d5cf3584fdda421982370aea386d
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-25 19:41:52 +0100

    refactor: simplify Age parsing logic using functional programming for improved readability

--- day-25/src/Age.cs
+++ day-25/src/Age.cs
@@ -1,11 +1,12 @@
 using LanguageExt;
+using static LanguageExt.Prelude;
 
 namespace GifFeedbackAudit;
 
 public record Age(int Value)
 {
     public static Option<Age> Parse(string input)
-        => Prelude.parseInt(input)
+        => parseInt(input)
             .Filter(age => age > 0)
             .Map(age => new Age(age));
 }
